e056f28096cacc9fbc8a73c8fb06af36ea2c6b0a
Beatriz Rizental <beatriz.rizental@gmail.com>
2018-05-15 13:12:07 -0300

    Kick start para o Exercicio 02

---
+++ 02/main.js
@@ -0,0 +1,50 @@
+var appointments = {
+  "06h30": {
+    "title": "HIIT",
+    "person": "Michael Goulart"
+  },
+  "07h00": {
+    "title": "Zumba",
+    "person": "Mariana Silva"
+  },
+  "07h30": {
+    "title": "Power Jump",
+    "person": "Alvaro Bigaton"
+  },
+  "08h00": {
+    "title": "Fit Dance",
+    "person": "Victor Bonifácio"
+  },
+  "08h30": {
+    "title": "Gap",
+    "person": "Michael Goulart"
+  },
+  "18h00": {
+    "title": "Power Jump",
+    "person": "Alvaro Bigaton"
+  },
+  "18h30": {
+    "title": "Zumba",
+    "person": "Mariana Silva"
+  },
+  "19h00": {
+    "title": "HIIT",
+    "person": "Victor Bonifácio"
+  },
+  "19h00": {
+    "title": "Jiu-Jitsu",
+    "person": "Beto Almeida"
+  },
+  "19h30": {
+    "title": "Abdominal",
+    "person": "Michael Goulart"
+  },
+  "20h00": {
+    "title": "Fit Dance",
+    "person": "Evandro Almeida"
+  },
+  "20h30": {
+    "title": "Glúteos",
+    "person": "Mariana Silva"
+  }
+}
