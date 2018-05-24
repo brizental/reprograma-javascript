a68ae7d8803f5740411cee5372fdf11494c07cc8
Beatriz Rizental <beatriz.rizental@gmail.com>
2018-05-24 16:18:13 -0300

    Exercicio 08, quiz funcionando

--- 08/main.js
+++ 08/main.js
@@ -14,19 +14,26 @@ let questions = [
   }
 ]
 
+const questionsAll = document.getElementById("questions")
 const closeButton = document.getElementById("close")
 const startButton = document.querySelector("#container button")
 const overlay = document.getElementById("overlay")
 const timer = document.getElementById("timer")
 let interval = undefined
 startButton.addEventListener("click", () => {
+  console.log("clicouuu")
   overlay.style.top = "0vh"
   interval = startTimer(timer)
 })
-closeButton.addEventListener("click", () => {
+closeButton.addEventListener("click", closeQuiz)
+
+function closeQuiz() {
+  setTimeout(function () {
+    questionsAll.style.left = "0vw"
+  }, 500)
   overlay.style.top = "100vh"
-  clearInterval(setInterval)
-})
+  clearInterval(interval)
+}
 
 function startTimer(element) {
   element.innerHTML = "00:00"
@@ -40,4 +47,48 @@ function startTimer(element) {
       ? "0" + diffDate.getSeconds() : diffDate.getSeconds()
     element.innerHTML = minutes + ":" + seconds
   }, 1000)
+}
+
+for (let i = 0; i < questions.length; i++) {
+  const questionDiv = document.createElement("div")
+  questionDiv.classList.add("question")
+  questionDiv.style.left = 100 * i + "vw"
+
+  const questionP = document.createElement("p")
+  questionP.textContent = questions[i].text
+
+  const answersDiv = document.createElement("div")
+  answersDiv.classList.add("answers")
+
+  const trueButton = document.createElement("button")
+  trueButton.textContent = "VERDADEIRO"
+  const falseButton = document.createElement("button")
+  falseButton.textContent = "FALSO"
+
+  questionDiv.appendChild(questionP)
+
+  answersDiv.appendChild(trueButton)
+  answersDiv.appendChild(falseButton)
+
+  questionDiv.appendChild(answersDiv)
+
+  questionsAll.appendChild(questionDiv)
+
+  trueButton.addEventListener("click", function() {
+    questions[i].userAnswer = true
+    if (i < questions.length - 1) {
+      questionsAll.style.left = `calc(${questionsAll.offsetLeft}px - 100vw)`
+    } else {
+      closeQuiz()
+    }
+  })
+
+  falseButton.addEventListener("click", function() {
+    questions[i].userAnswer = false
+    if (i < questions.length - 1) {
+      questionsAll.style.left = `calc(${questionsAll.offsetLeft}px - 100vw)`
+    } else {
+      closeQuiz()
+    }
+  })
 }
